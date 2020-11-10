fe2b42b4bb14267923f6f13a2cf32e865e362df1
RifatShamsuddin <60315803+RifatShamsuddin@users.noreply.github.com>
2020-11-11 01:07:01 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -147,4 +147,7 @@ Query3: SELECT employee_id, last_name, email, commission_pct, department_id FROM
 Query4: SELECT employee_id, last_name, email, commission_pct, department_id FROM employees WHERE department_id =5 AND commission_pct>ALL (SELECT commission_pct FROM employees WHERE department_id=7);
 Query5: SELECT employee_id, last_name, email, salary, department_id FROM employees WHERE department_id =5 AND commission_pct>ANY (SELECT salary FROM employees WHERE department_id=7);
 Query6: SELECT department_id, job_id, salary FROM employees WHERE salary<ANY (SELECT job_id, salary FROM employees GROUP BY department_id);
-Query7:
+Query7: SELECT manager_id FROM employees L1 WHERE EXISTS (SELECT * FROM employees L2 Where L2.manager_id=L1.manager_id AND L2.salary<2500);
+Query8: SELECT manager_id FROM employees L1 WHERE EXISTS (SELECT * FROM employees L2 Where L2.manager_id=L1.manager_id AND L2.commission_pct<18.25);
+Query9: SELECT manager_id FROM employees L1 WHERE NOT EXISTS (SELECT * FROM employees L2 Where L2.manager_id=L1.manager_id AND L2.salary<3500);
+Query10:SELECT manager_id FROM employees L1 WHERE NOT EXISTS (SELECT * FROM employees L2 Where L2.manager_id=L1.manager_id AND L2.commission_pct<45.05);
